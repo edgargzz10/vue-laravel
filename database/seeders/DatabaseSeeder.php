8a9bca3f02917ca63c16744de886eb8614093950
name <edgar.gzz10@gmail.com>
2024-06-09 11:22:14 -0600

    Creating Roleas & Permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-       $this->call(LevelSeeder::class);
+       $this->call([LevelSeeder::class,
+                    RoleSeeder::class,
+                    UserSeeder::class]);       
     }
 }
